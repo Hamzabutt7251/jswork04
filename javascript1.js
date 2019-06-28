7c157bd6b0742bb96887e950e63e77a8344dc71f
hamzabutt7251 <hamzabutt7251@gmail.com>
2019-06-28 12:57:50 +0500

    first new0 commit

---
+++ javascript1.js
@@ -0,0 +1,257 @@
+
+// var a = 4; if(a===4){a++}
+
+// var b = 3; if(b===3){a++}
+
+// var d = 5; if(d===5){d++}
+
+// var e = 5; if(e===5){e++}
+
+// var z = 5; if(z===5){z++}
+
+// var b = 5; if(b===5){b++}
+
+// var n = 9; if(n===9){n++}
+
+// var t = 15; if(t===15){t++}
+
+// var u = 19;if(u===19){u++}
+
+// var g = 20;if(g===20){g++}
+
+
+
+var name = prompt("Enter your name");
+ if(name==="hammad")
+
+ {alert("you are hammad")}
+
+ else 
+ {
+console.log("ap kon ha");
+ }
+
+
+
+//  var marks = prompt("enter the marks");
+//  if(marks==="50")
+//  {
+//      document.write("you are pass.");
+//  }
+//  if(marks==="60") 
+//  {
+//      document.write("you are pass.");
+//  }
+//  else if(marks==="70")
+//  {
+//      document.write("you are selected.");
+//  }
+//  else if(marks==="80")
+//  {
+//      document.write("you are selected.");
+//  }
+//  else
+//  {
+//      document.write("you are not selected.");
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  var marks = prompt("Enter the percentage");
+//  if(marks==="50%")
+//  {
+//      document.write("your percentage is passed");
+//  }
+//  if(marks==="60%")
+//  {
+//      document.write("your percentage is passed");
+//  }
+//  else if(marks==="40%")
+// {
+//     document.write("your percentage is failed");
+
+// }
+// else if(marks==="20")
+// {
+//     document.write("your percentage is failed");
+// }
+// else
+// {
+//     document.write("your percentage is failed")
+// }
+
+
+
+
+// var percentage = prompt("enter your percentage")
+// if(percentage==="50")
+// {
+//     document.write("you are passed");
+// }
+// if(percentage==="60")
+// {
+//     document.write("you are passed");
+// }
+// else if(percentage==="40")
+// {
+//     document.write("you are passed");
+// } 
+// else if(percentage<"30")
+// {
+// document.write("you are passed");
+// }
+// else if(percentage==="20")
+// {
+//     document.write("you are not passed");
+// }
+// else
+// {
+//  document.write("you are failed");
+// }
+
+
+
+
+// var rollno = prompt("Enter the roll no");
+// if(rollno>"100")
+// {
+//     document.write("you are selected");
+// }
+// if(rollno<"99")
+// {
+//     document.write("you are not selected");
+// }
+// else if(rollno>"50")
+// {
+//     document.write("you are selected")
+// }
+// else{
+//     document.write("you are failed");
+// }
+
+
+
+
+// var book= prompt("please enter the book");
+
+// if(book==="computer science")
+// {
+//     document.write("you are studying on the cs department");
+// }
+// if(book==="physics")
+// {
+//     document.write("you are stydying int he physics department");
+// }
+// else if(book==="math")
+// {
+//     document.write("you are studing in the maths department");
+// }
+// else if(book==="urdu")
+// {
+//     document.write("you are studying in the urdu department");
+// }
+// else{
+//     document.write("you are studying in the othe department");
+// }
+
+
+
+
+
+// var book= prompt("please enter the book");
+
+// if(book==="computer science")
+// {
+//     document.write("you are studying on the cs department");
+// }
+// if(book==="physics")
+// {
+//     document.write("you are stydying int he physics department");
+// }
+// else if(book==="math")
+// {
+//     document.write("you are studing in the maths department");
+// }
+// else if(book==="urdu")
+// {
+//     document.write("you are studying in the urdu department");
+// }
+// else{
+//     document.write("you are studying in the othe department");
+// }
+
+
+
+// var a = "50";
+// if (a===50);
+// {
+// document.write("you are selected in the test")''
+// }
+// if(a===40)
+// {
+//     document.write("you are selected in the test");
+// }
+// else if(a>40)
+// document.write("you are not selcted in the exam");
+// else{
+// ("you are failed");
+// }
+
+
+
+// var product= "soap";
+// if(soap==="dove")
+// {
+//     document.write("yes itis avilable");
+// }
+// if(soap==="lux")
+// {
+//     document.write("yes it is avilable");
+// }
+// else if(soap==="berry")
+// {
+//     document.write("out of stocl");
+// }
+// else if(soap==="lifebouy")
+// {
+//     document.write("out of stock");
+// }
+// else
+// {
+// Document.write("FINISHED";)
+// }
+
+
+
+
+
+// var toothpaste = prompt("enter the name");
+
+// if(toothpaste==="closeup")
+// {alert("avilable in the stock")};
+// if(toothpaste==="medicam")
+// {
+//     alert("avillble in the stock");
+// }
+// else if
+// {
+//     alert("avialbe in the stock");
+// }
+// else if
+// {
+//     alert("avialble inthe market");
+// }
+// else
+// {
+//     alert("out of stock");
+// }
